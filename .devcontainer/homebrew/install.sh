e6acf691ba841e51dcf2caae84068a2f79d6b320
Denys Bushuliak <denys.b@devbrother.com>
2025-08-28 08:53:42 +0000

    remove homebrew but add node with npm

--- .devcontainer/homebrew/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export NONINTERACTIVE=1
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-sudo apt-get install build-essential
-brew install gcc node
