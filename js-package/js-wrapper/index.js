5f78fe16693961fa3568a7ea017e01cb872bbae3
OleksandrK-DevBrother <oleksandr.kyr@devbrother.com>
2025-09-02 18:25:39 +0200

    fixed package bundling

--- js-package/js-wrapper/index.js
+++ js-package/js-wrapper/index.js
@@ -1,17 +1,12 @@
 import initWasm, * as wasm from "../src/graph_to_rholang_parser.js";
+import wasmUrl from "../src/graph_to_rholang_parser_bg.wasm?url";
 
 let _ready = null;
 
-async function init() {
-    if (!_ready) {
-        import wasmUrl from "../src/graph_to_rholang_parser_bg.wasm?url";
-        _ready = initWasm(wasmUrl);
-    }
+export async function init() {
+    if (!_ready) _ready = initWasm(wasmUrl);
     return _ready;
 }
-
-export { init };
-export default init;
 
 export function parse_to_ast(input) {
     if (!_ready) throw new Error("graphl-parser not initialized. Call await init() first.");
